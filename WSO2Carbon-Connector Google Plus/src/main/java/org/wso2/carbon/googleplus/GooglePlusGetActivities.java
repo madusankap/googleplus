48192aa12e8dd56f8d72066690ba6e9575915b14
tharindud <ghtd.dharmarathna@gmail.com>
2014-08-30 11:34:00 +0530

    changes 30/08/2014

--- WSO2Carbon-Connector Google Plus/src/main/java/org/wso2/carbon/googleplus/GooglePlusGetActivities.java
+++ WSO2Carbon-Connector Google Plus/src/main/java/org/wso2/carbon/googleplus/GooglePlusGetActivities.java
@@ -2,10 +2,14 @@ package org.wso2.carbon.googleplus;
 
 import com.google.api.services.plus.Plus;
 import com.google.api.services.plus.model.Activity;
+import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.synapse.MessageContext;
 import org.wso2.carbon.connector.core.AbstractConnector;
 import org.wso2.carbon.connector.core.ConnectException;
 
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,19 +26,9 @@ public class GooglePlusGetActivities extends AbstractConnector {
         if(!("").equals(activityid)){
                         try {
                             if(null==fields || ("").equals(fields)){
-                                Plus plus= GoogleplusUtil.getPlusServices(messageContext);
-                                Activity activity = plus.activities().get(activityid).setPrettyPrint(true).execute();
-                                System.out.println("else\n"+activity.toString());
-                                resultEnvelopeMap.put(GoogleplusUtil.StringConstants.Activity,activity.toString());
-                                messageContext.setEnvelope(GoogleplusUtil.buildResultEnvelope(GoogleplusUtil.StringConstants.URN_GET_ACTIVITY,GoogleplusUtil.StringConstants.GET_Activity,resultEnvelopeMap));
+                              getActivity(messageContext,activityid);
                             }else{
-
-                                Plus plus= GoogleplusUtil.getPlusServices(messageContext);
-                                Activity activity = plus.activities().get(activityid).setPrettyPrint(true).setFields(fields).execute();
-                                System.out.println("if:\n"+activity.toString());
-                                resultEnvelopeMap.put(GoogleplusUtil.StringConstants.Activity,activity.toString());
-                                messageContext.setEnvelope(GoogleplusUtil.buildResultEnvelope(GoogleplusUtil.StringConstants.URN_GET_ACTIVITY,GoogleplusUtil.StringConstants.GET_Activity,resultEnvelopeMap));
-
+                                getActivity(messageContext,activityid,fields);
                             }
 
             }catch (Exception ex){
@@ -42,5 +36,21 @@ System.out.println(ex.getMessage());
                          }
         }
 
+    }
+    void getActivity(MessageContext messageContext,String activityid) throws IOException, GeneralSecurityException, XMLStreamException {
+        Plus plus= GoogleplusUtil.getPlusServices(messageContext);
+        Activity activity = plus.activities().get(activityid).setPrettyPrint(true).execute();
+        resultEnvelopeMap.put(GoogleplusUtil.StringConstants.Activity, activity.toPrettyString());
+        messageContext.getEnvelope().detach();
+        SOAPEnvelope soapEnvelope=GoogleplusUtil.buildResultEnvelope(GoogleplusUtil.StringConstants.URN_GET_ACTIVITY,GoogleplusUtil.StringConstants.GET_Activity,resultEnvelopeMap);
+        messageContext.setEnvelope(soapEnvelope);
+    }
+    void getActivity(MessageContext messageContext,String activityid,String fields) throws IOException, GeneralSecurityException, XMLStreamException {
+        Plus plus= GoogleplusUtil.getPlusServices(messageContext);
+        Activity activity = plus.activities().get(activityid).setPrettyPrint(true).setFields(fields).execute();
+        resultEnvelopeMap.put(GoogleplusUtil.StringConstants.Activity,activity.toPrettyString());
+        messageContext.getEnvelope().detach();
+        SOAPEnvelope soapEnvelope=GoogleplusUtil.buildResultEnvelope(GoogleplusUtil.StringConstants.URN_GET_ACTIVITY,GoogleplusUtil.StringConstants.GET_Activity,resultEnvelopeMap);
+        messageContext.setEnvelope(soapEnvelope);
     }
 }
